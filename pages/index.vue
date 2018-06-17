5d8ddc7bd72e68a894dc46a54e9077004ddf5087
davidrfreeman <david.freeman12@gmail.com>
2018-06-17 10:50:22 -0600

    Add NavBar component

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,38 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        Trinity Lutheran School
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <div>
+    <nav-bar></nav-bar>
+    <section class="container">
+      <div>
+        <app-logo/>
+        <h1 class="title">
+          Trinity Lutheran School
+        </h1>
+        <h2 class="subtitle">
+          Nuxt.js project
+        </h2>
+        <div class="links">
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            class="button--green">Documentation</a>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            class="button--grey">GitHub</a>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 <script>
 import AppLogo from '~/components/AppLogo.vue'
+import NavBar from '~/components/NavBar.vue'
 
 export default {
   components: {
-    AppLogo
+    AppLogo,
+    NavBar
   }
 }
 </script>
