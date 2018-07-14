0069ebab6e64820e2048d8a69898b5f5a101fd47
davidrfreeman <david.freeman12@gmail.com>
2018-07-14 09:18:45 -0600

    Add chruch img and jumbotron to index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1 class="title">Trinity Lutheran School</h1>
+    <b-jumbotron header="Bootstrap Vue" lead="Bootstrap 4 Components for Vue.js 2" >
+      <p>For more information visit website</p>
+    </b-jumbotron>
   </div>
 </template>
 
@@ -11,26 +13,9 @@ export default {
 }
 </script>
 
-<style>
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style scoped>
+  .jumbotron {
+    /* background-image: url('~/assets/church.png'); */
+    height: 100vh;
+  }
 </style>
