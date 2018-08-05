36e96562ffd230bc7cab7f33a8dc7a0b55a0c317
davidrfreeman <david.freeman12@gmail.com>
2018-08-05 13:01:51 -0600

    Fix spelling error

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <b-jumbotron >
       <h1 class="jumbo_text display-3">Trinity Lutheran</h1>
-      <h2 class="jumbo_text display-4">Church, Shchool, and Early Learning Center</h2>
+      <h2 class="jumbo_text display-4">Church, School, and Early Learning Center</h2>
     </b-jumbotron>
   </div>
 </template>
@@ -13,13 +13,13 @@ export default {}
 
 <style scoped>
 .jumbotron {
-  /* background-image: url('~/assets/church.png'); */
-  height: 93.5vh;
+  height: 100vh;
   background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
     url("../assets/church.png");
   opacity: 0.8;
   background-size: cover;
 }
+
 .jumbo_text {
   color: #fff;
 }
